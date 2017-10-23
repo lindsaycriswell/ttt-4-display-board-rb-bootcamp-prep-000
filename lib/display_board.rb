964a2610b5d63eb7c4723a67d646689c3480babe
Lindsay Criswell <lindsaycriswell@gmail.com>
2017-10-23 19:05:00 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board
+  break = "-----------"
+
+  puts " #{display_board[0]} | #{display_board[1]} | #{display_board[2]} "
+  puts break
+  puts " #{display_board[3]} | #{display_board[4]} | #{display_board[5]} "
+  puts break
+  puts " #{display_board[6]} | #{display_board[7]} | #{display_board[8]} "
+end
